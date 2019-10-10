9c59baa914522f58c4efdf1d6a3b1b72e737a3e3
VasilchenkoSergei <chainsaw86@mail.ru>
2019-10-10 08:49:22 +0300

    правка стилей и смена шрифта

--- src/blocks/main-card/main-card.js
+++ src/blocks/main-card/main-card.js
@@ -1,19 +1,28 @@
 
-if (window.innerWidth < 1024) {
+const cardOpen = function() {
   const mainCardHeaders = document.querySelectorAll('.main-card__title');
-
+  
   mainCardHeaders.forEach(header => {
     header.addEventListener('click', (e) => {
       const goToHeader = e.target.closest('div');
       mainCardHeaders.forEach(item => {
         item.closest('div').classList.remove('active');
       })
-
+  
       if (goToHeader.classList.contains('active')) {
         goToHeader.classList.remove('active');
       } else {
         goToHeader.classList.add('active');
       }
     });
+  });
+}
+
+if (window.innerWidth < 1024) {
+  cardOpen();
+}
+if (window.innerWidth < 1200) {
+  window.addEventListener('resize', () => {
+    cardOpen();
   });
 }
--- src/blocks/search/search.js
+++ src/blocks/search/search.js
@@ -1,21 +1,31 @@
-const searchInput = document.querySelector('.search input');
+const searchInput = document.querySelector('#search-input');
 
 searchInput.addEventListener('input', () => {
-  let text = searchInput.value;
 
-  var source = document.querySelectorAll('.main-card__body');
+  
+  
+    var lastResFind=""; // последний удачный результат
+    var copy_page=""; // копия страницы в ихсодном виде
+    function TrimStr(s) {
+        s = s.replace( /^\s+/g, '');
+      return s.replace( /\s+$/g, '');
+    }
+    function FindOnPage(searchInput) {//ищет текст на странице, в параметр передается ID поля для ввода
+      var obj = window.document.getElementById(searchInput);
+      var textToFind;
+      
+      if (obj) {
+        textToFind = TrimStr(obj.value);//обрезаем пробелы
+      } 
 
-  source.forEach(item => {
-    const innerText = item.innerHTML
-    if(innerText.search(text) !== -1){
-      // console.log(innerText.search(text));
       
-    } else {
-      // console.log(7787897897897987);
+      if(document.body.innerHTML.indexOf(textToFind)=="-1")
+      alert("Ничего не найдено, проверьте правильность ввода!");
       
+      if(copy_page.length > 0) {
+        document.body.innerHTML = copy_page
+      } else { copy_page = document.body.innerHTML }
     }
 
-  })
-  
-  
+    FindOnPage('search-input')
 });
